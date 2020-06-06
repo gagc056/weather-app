fb7714d03cd66b165415c132b5d14dd58793807d
lhscar <gagc056@gmail.com>
2020-06-06 00:53:11 -0300

    adding buttons celsius and farenheit

--- src/UI.js
+++ src/UI.js
@@ -15,9 +15,27 @@ export default class UI {
       this.location.textContent = `${weather.name}  `;
       this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png `);
       this.description.textContent = weather.weather[0].description;
-      this.string.textContent = `${weather.main.temp}°C`;
+      this.string.innerHTML = `<p id='temp'>${weather.main.temp}°C</p><button data-metric="C" data-value="${weather.main.temp}" id='changeMetric'>°F </button>`;
       this.wind.textContent = `Wind:${weather.wind.speed}m/s`;
       this.humidity.textContent = `Humidity: ${weather.main.humidity} °C`;
+
+      document.getElementById('changeMetric').addEventListener('click', e => {
+        if (e.target.getAttribute('data-metric') === 'C') {
+          e.target.setAttribute('data-metric', 'F');
+          const value = e.target.getAttribute('data-value');
+          const temp = Math.floor((value * 9) / 5 + 32);
+          e.target.setAttribute('data-value', temp);
+          this.string.querySelector('#temp').textContent = `${Math.floor((temp * 9) / 5 + 32)}°F`;
+          e.target.textContent = 'C';
+        } else {
+          e.target.setAttribute('data-metric', 'C');
+          const value = e.target.getAttribute('data-value');
+          const temp = Math.floor((value - 32) * (5 / 9));
+          e.target.setAttribute('data-value', temp);
+          this.string.querySelector('#temp').textContent = `${temp}°C`;
+          e.target.textContent = 'F';
+        }
+      });
     }
   }
 }
