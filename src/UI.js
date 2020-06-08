ad52d210c892ff2ae198daa7e8003c78c642ca30
lhscar <gagc056@gmail.com>
2020-06-08 00:40:47 -0300

    I add math. round to fix my problem

--- src/UI.js
+++ src/UI.js
@@ -15,7 +15,8 @@ export default class UI {
       this.location.textContent = `${weather.name}  `;
       this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png `);
       this.description.textContent = weather.weather[0].description;
-      this.string.innerHTML = `<p id='temp'>${weather.main.temp}°C</p><button data-metric="C" data-value="${weather.main.temp}" id='changeMetric'>°F </button>`;
+      this.string.textContent = Math.floor(weather.main.temp);
+      this.string.innerHTML = `<p id='temp'>${Math.round(weather.main.temp)}°C</p><button data-metric="C" data-value="${weather.main.temp}" id='changeMetric'>°F </button>`;
       this.wind.textContent = `Wind:${weather.wind.speed}m/s`;
       this.humidity.textContent = `Humidity: ${weather.main.humidity} °C`;
 
@@ -23,14 +24,14 @@ export default class UI {
         if (e.target.getAttribute('data-metric') === 'C') {
           e.target.setAttribute('data-metric', 'F');
           const value = e.target.getAttribute('data-value');
-          const temp = Math.floor((value * 9) / 5 + 32);
+          const temp = Math.round((value * 9) / 5 + 32);
           e.target.setAttribute('data-value', temp);
-          this.string.querySelector('#temp').textContent = `${Math.floor((temp * 9) / 5 + 32)}°F`;
+          this.string.querySelector('#temp').textContent = `${Math.round((temp * 9) / 5 + 32)}°F`;
           e.target.textContent = 'C';
         } else {
           e.target.setAttribute('data-metric', 'C');
           const value = e.target.getAttribute('data-value');
-          const temp = Math.floor((value - 32) * (5 / 9));
+          const temp = Math.round((value - 32) * (5 / 9));
           e.target.setAttribute('data-value', temp);
           this.string.querySelector('#temp').textContent = `${temp}°C`;
           e.target.textContent = 'F';
